4036b5bd9df8f5d23ce549c5330d50b871f4b3a3
Ognyan Genev <ognian.genev@gmail.com>
2021-10-02 15:18:02 +0300

    Initial portal state and portal history json-rpc specs

---
+++ jsonrpc/scripts/build.js
@@ -0,0 +1,69 @@
+import fs from "fs";
+import { parseOpenRPCDocument } from "@open-rpc/schema-utils-js";
+
+console.log("Loading files...\n");
+
+let methods = [];
+let methodsBase = "src/methods/";
+let methodFiles = fs.readdirSync(methodsBase);
+methodFiles.forEach(file => {
+  console.log(file);
+  let raw = fs.readFileSync(methodsBase + file);
+  let parsed = JSON.parse(raw);
+  methods = [
+    ...methods,
+    ...parsed,
+  ];
+});
+
+let schemas = {};
+let schemasBase = "src/schemas/"
+let schemaFiles = fs.readdirSync(schemasBase);
+schemaFiles.forEach(file => {
+  console.log(file);
+  let raw = fs.readFileSync(schemasBase + file);
+  let parsed = JSON.parse(raw);
+  schemas = {
+    ...schemas,
+    ...parsed,
+  };
+});
+
+
+let content = {};
+let contentBase = "src/content/"
+let contentFiles = fs.readdirSync(contentBase);
+contentFiles.forEach(file => {
+  console.log(file);
+  let raw = fs.readFileSync(contentBase + file);
+  let parsed = JSON.parse(raw);
+  content = {
+    ...content,
+    ...parsed,
+  };
+});
+
+let spec = await parseOpenRPCDocument({
+  openrpc: "1.2.4",
+  info: {
+    title: "Portal Network JSON-RPC Specification",
+    description: "A specification of the standard interface for Portal Network clients.",
+    license: {
+      name: "CC0-1.0",
+      url: "https://creativecommons.org/publicdomain/zero/1.0/legalcode"
+    },
+    version: "0.0.1"
+  },
+  methods: methods,
+  components: {
+    contentDescriptors: content,
+    schemas: schemas
+  }
+},
+    {dereference: false})
+
+let data = JSON.stringify(spec, null, '\t');
+fs.writeFileSync('openrpc.json', data);
+
+console.log();
+console.log("Build successful.");
