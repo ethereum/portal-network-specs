66a61e520a9a0727b07ec99bfdd3335888cc2169
Piper Merriam <piper@pipermerriam.com>
2021-12-22 15:18:41 -0700

    d

--- w3_debug_trace.py
+++ w3_debug_trace.py
@@ -12,6 +12,7 @@ from typing import (
 
 from toolz import groupby, concat
 from eth_utils import (
+    to_int,
     to_tuple,
     to_canonical_address,
     decode_hex,
@@ -24,6 +25,7 @@ from eth_typing import (
 from hexbytes import (
     HexBytes,
 )
+from eth.vm import mnemonics
 
 from web3 import Web3
 from web3.method import (
@@ -97,7 +99,7 @@ def _block_trace_formatter(raw_traces: Sequence[RawTraceResult]) -> Tuple[Trace,
                     gasCost=sl['gasCost'],
                     depth=sl['depth'],
                     stack=tuple(
-                        decode_hex(item) for item in sl['stack']
+                        to_int(hexstr=item) for item in sl['stack']
                     ),
                 ) for sl in rt['result']['structLogs']
             ),
@@ -144,16 +146,153 @@ class TxMeta(NamedTuple):
 
 
 CALL_OPS = {
-    "CALL",
+    mnemonics.CALL,
+    mnemonics.CALLCODE,
+    mnemonics.STATICCALL,
+    mnemonics.DELEGATECALL,
 }
 BENIGN_OPS = {
-    "STOP",
+    # Arithmetic
+    mnemonics.STOP,
+    mnemonics.ADD,
+    mnemonics.MUL,
+    mnemonics.SUB,
+    mnemonics.DIV,
+    mnemonics.SDIV,
+    mnemonics.MOD,
+    mnemonics.SMOD,
+    mnemonics.ADDMOD,
+    mnemonics.MULMOD,
+    mnemonics.EXP,
+    mnemonics.SIGNEXTEND,
+    mnemonics.SHL,
+    mnemonics.SHR,
+    mnemonics.SAR,
+    # Comparisons
+    mnemonics.LT,
+    mnemonics.GT,
+    mnemonics.SLT,
+    mnemonics.SGT,
+    mnemonics.EQ,
+    mnemonics.ISZERO,
+    mnemonics.AND,
+    mnemonics.OR,
+    mnemonics.XOR,
+    mnemonics.NOT,
+    mnemonics.BYTE,
+    # SHA
+    mnemonics.SHA3,
+    # Environment
+    mnemonics.ADDRESS,
+    mnemonics.CALLER,
+    # BLOCK
+    mnemonics.COINBASE,
+    mnemonics.TIMESTAMP,
+    mnemonics.NUMBER,
+    mnemonics.DIFFICULTY,
+    mnemonics.GASLIMIT,
+    mnemonics.BASEFEE,
+    # PC
+    mnemonics.PC,
+    # Memory
+    mnemonics.MSTORE,
+    mnemonics.MSTORE8,
+    mnemonics.MLOAD,
+    mnemonics.MSIZE,
+    # System
+    mnemonics.CALLDATALOAD,
+    mnemonics.CALLDATASIZE,
+    mnemonics.CALLDATACOPY,
+    mnemonics.RETURNDATASIZE,
+    mnemonics.RETURNDATACOPY,
+    mnemonics.GAS,
+    mnemonics.JUMP,
+    mnemonics.JUMPI,
+    mnemonics.JUMPDEST,
+    mnemonics.STOP,
+    mnemonics.RETURN,
+    mnemonics.REVERT,
+    # DUP
+    mnemonics.DUP1,
+    mnemonics.DUP2,
+    mnemonics.DUP3,
+    mnemonics.DUP4,
+    mnemonics.DUP5,
+    mnemonics.DUP6,
+    mnemonics.DUP7,
+    mnemonics.DUP8,
+    mnemonics.DUP9,
+    mnemonics.DUP10,
+    mnemonics.DUP11,
+    mnemonics.DUP12,
+    mnemonics.DUP13,
+    mnemonics.DUP14,
+    mnemonics.DUP15,
+    mnemonics.DUP16,
+    # LOG
+    mnemonics.LOG0,
+    mnemonics.LOG1,
+    mnemonics.LOG2,
+    mnemonics.LOG3,
+    mnemonics.LOG4,
+    # POP
+    mnemonics.POP,
+    # PUSH
+    mnemonics.PUSH1,
+    mnemonics.PUSH2,
+    mnemonics.PUSH3,
+    mnemonics.PUSH4,
+    mnemonics.PUSH5,
+    mnemonics.PUSH6,
+    mnemonics.PUSH7,
+    mnemonics.PUSH8,
+    mnemonics.PUSH9,
+    mnemonics.PUSH10,
+    mnemonics.PUSH11,
+    mnemonics.PUSH12,
+    mnemonics.PUSH13,
+    mnemonics.PUSH14,
+    mnemonics.PUSH15,
+    mnemonics.PUSH16,
+    mnemonics.PUSH17,
+    mnemonics.PUSH18,
+    mnemonics.PUSH19,
+    mnemonics.PUSH20,
+    mnemonics.PUSH21,
+    mnemonics.PUSH22,
+    mnemonics.PUSH23,
+    mnemonics.PUSH24,
+    mnemonics.PUSH25,
+    mnemonics.PUSH26,
+    mnemonics.PUSH27,
+    mnemonics.PUSH28,
+    mnemonics.PUSH29,
+    mnemonics.PUSH30,
+    mnemonics.PUSH31,
+    mnemonics.PUSH32,
+    # SWAP
+    mnemonics.SWAP1,
+    mnemonics.SWAP2,
+    mnemonics.SWAP3,
+    mnemonics.SWAP4,
+    mnemonics.SWAP5,
+    mnemonics.SWAP6,
+    mnemonics.SWAP7,
+    mnemonics.SWAP8,
+    mnemonics.SWAP9,
+    mnemonics.SWAP10,
+    mnemonics.SWAP11,
+    mnemonics.SWAP12,
+    mnemonics.SWAP13,
+    mnemonics.SWAP14,
+    mnemonics.SWAP15,
+    mnemonics.SWAP16,
 }
 READ_OPS = {
-    "SLOAD",
+    mnemonics.SLOAD,
 }
 WRITE_OPS = {
-    "SSTORE",
+    mnemonics.SSTORE,
 }
 
 
@@ -168,35 +307,43 @@ class CallContext(NamedTuple):
 @to_tuple
 def get_access_events(
         tx_meta: TxMeta,
-        trace_entries: Sequence[TraceEntry]) -> Iterable[StateAccess]:
+        trace: Trace) -> Iterable[StateAccess]:
     yield StateAccess(tx_meta.sender)
+
     if tx_meta.to is None:
         assert False
     else:
         yield StateAccess(tx_meta.to)
 
-    call_context = CallContext(
+    context = CallContext(
+        sender=tx_meta.sender,
         self=tx_meta.to,
         storage=tx_meta.to,
         code=tx_meta.to,
         value=tx_meta.value,
     )
 
-    for entry in trace_entries:
+    stack = []
+
+    for idx, entry in enumerate(trace.structLogs):
         if entry.op in BENIGN_OPS:
             continue
         elif entry.op in READ_OPS:
-            assert False
+            if entry.op == mnemonics.SLOAD:
+                yield StateAccess(context.storage, entry.stack[-1])
+            else:
+                assert False, f"not handled: op={entry.op}"
         elif entry.op in WRITE_OPS:
-            assert False
+            assert False, f"not handled: op={entry.op}"
         elif entry.op in CALL_OPS:
-            assert False
+            assert False, f"not handled: op={entry.op}"
         else:
             raise ValueError(f"Unsupported opcode: op={entry.op}")
 
 
-def get_block_access_list(w3: Web3, block_identifier: BlockIdentifier) -> AccessList:
-    block_trace = w3.debug_trace.trace_block(block_identifier)
+def get_block_access_list(w3: Web3, block_identifier: BlockIdentifier, trace: Optional[Trace] = None) -> AccessList:
+    if trace is None:
+        trace = w3.debug_trace.trace_block(block_identifier)
     block = w3.eth.get_block(block_identifier, full_transactions=True)
 
     tx_metas = tuple(
@@ -206,14 +353,14 @@ def get_block_access_list(w3: Web3, block_identifier: BlockIdentifier) -> Access
             value=tx['to'],
             data=tx['input'],
             gas=tx['gas'],
-            gas_priice=tx['gasPrice'],
+            gas_price=tx['gasPrice'],
             nonce=tx['nonce'],
         ) for tx in block['transactions']
     )
     access_events_by_tx = tuple(
         get_access_events(tx_meta, tx_trace)
         for tx_meta, tx_trace
-        in zip(tx_metas, block_trace['results'])
+        in zip(tx_metas, trace)
     )
     block_access_events = tuple(concat(access_events_by_tx))
     access_event_groups = groupby('address', block_access_events)
